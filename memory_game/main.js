4308a3166cadd9e19613c54e399d241cb5bc091b
alexandraalday <alexandra.alday@gmail.com>
2017-05-18 13:46:21 -0700

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -2,22 +2,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-//Add first card user flipped to cardsInPlay array and display to console
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + " " + cardOne);
+//Check for match
+var checkForMatch = function() {
+	 if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+  } else {
+      alert("Sorry, try again.");
+  }
+ };
 
-//Add next card user flipped to cardsInPlay array and display to console
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + " " + cardTwo);
+//Select cards
+var flipCard = function(cardId) {
 
-//Check if user played two equal cards
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1] ) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.")
-	}
-}
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
